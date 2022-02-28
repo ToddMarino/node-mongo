438d9397ca3d8e2bfdcff8e339515d9847b548d5
ToddMarino <todd.j.marino@gmail.com>
2022-02-28 13:34:50 -0600

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert").strict;
 const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017";
@@ -7,54 +6,61 @@ const dbname = "nucampsite";
 
 // series of 3 nested callbacks used because of async operation
 // typically, nested callbacks is not the preferred method
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  // same as using if(err === null) then quit. otherwise continue. More abrupt way of error handling
-  assert.strictEqual(err, null); // if err !== null, quit the application
-  console.log("Connected correctly to server");
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected correctly to server");
 
-  const db = client.db(dbname);
+    const db = client.db(dbname);
 
-  // delete all documents in campsite collection.
-  // this is only for testing purpose.
-  db.dropCollection("campsites", (err, result) => {
-    assert.strictEqual(err, null);
-    console.log("Dropped Collection", result);
+    db.dropCollection("campsites")
+      .then((result) => {
+        console.log("Dropped Collection:", result);
+      })
+      .catch((err) => console.log("No collection to drop."));
 
-    dboper.insertDocument(
-      db,
-      { name: "Breadcrumb Trail Campground", description: "Test" },
-      "campsites",
-      (result) => {
-        console.log("Insert Document", result.ops);
+    dboper
+      .insertDocument(
+        db,
+        { name: "Breadcrumb Trail Campground", description: "Test" },
+        "campsites"
+      )
+      .then((result) => {
+        console.log("Insert Document:", result.ops);
 
-        dboper.findDocuments(db, "campsites", (docs) => {
-          console.log("Found Documents:", docs);
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Documents:", docs);
 
-          dboper.updateDocument(
-            db,
-            { name: "Breadcrumb Trail Campground" },
-            { description: "Updated Test Description" },
-            "campsites",
-            (result) => {
-              console.log("Updated Document Count:", result.result.nModified);
+        return dboper.updateDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          { description: "Updated Test Description" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Updated Document Count:", result.result.nModified);
 
-              dboper.findDocuments(db, "campsites", (docs) => {
-                console.log("Found Documents:", docs);
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Documents:", docs);
 
-                dboper.removeDocument(
-                  db,
-                  { name: "Breadcrumb Trail Campground" },
-                  "campsites",
-                  (result) => {
-                    console.log("Deleted Document Count:", result.deletedCount);
-                    client.close();
-                  }
-                );
-              });
-            }
-          );
-        });
-      }
-    );
-  });
-});
+        return dboper.removeDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Deleted Document Count:", result.deletedCount);
+
+        return client.close();
+      })
+      .catch((err) => {
+        console.log(err);
+        client.close();
+      });
+  })
+  .catch((err) => console.log(err));
